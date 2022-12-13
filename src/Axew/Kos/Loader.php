e00f2bdb8a5c53e1497a6bafc8ee75cc16a3ba5e
Axew <95940690+Tweaky349@users.noreply.github.com>
2022-12-13 14:48:42 -0600

    Update Loader.php

--- src/Axew/Kos/Loader.php
+++ src/Axew/Kos/Loader.php
@@ -6,10 +6,6 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener; 
 use pocketmine\command\Command;
 use pocketmine\command\commandsender;
-use pocketmine\item\ItemIdentifier;
-use refaltor\customitemapi\CustomItemAPI;
-use refaltor\customitemapi\items\BaseItem;
-use Refaltor\CustomItemAPI\Items\BasicItem
 
 class Loader extends PluginBase implements Listener { 
 
@@ -19,8 +15,6 @@ class Loader extends PluginBase implements Listener {
 
     public function onLoad() : void{ 
         $this->getLogger()->info("Kos esta cargando"); 
-        $item = new BasicItem(new ItemIdentifier(1000,0),"Ejemplo", "apple", /*max stack size*/16);
-        $item->addToServer();
     } 
 
     public function onDisable() : void{ 
